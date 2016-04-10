7eb5e668d561282094290dbf3dcdaffe2d44b4e6
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-04-10 18:30:37 +0530

    Create Node.java

---
+++ UnionFindDataStructure/Node.java
@@ -0,0 +1,18 @@
+package abc;
+
+import java.util.ArrayList;
+
+public class Node {
+	ArrayList<Integer> set;
+	
+	public Node(ArrayList<Integer> set){
+		this.set = set;
+	}
+	public ArrayList<Integer> getSet(){
+		return this.set;
+	}
+	
+	public void setSet(ArrayList<Integer> al){
+		this.set = al;
+	}
+}
